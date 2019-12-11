7f8f6e0b4d97d5dbf8481f62ff62c65d8d6971ba
Michael Grizenko-Vida <grividm@gmail.com>
2019-12-11 01:14:12 -0500

    new state machine

--- countDownStateMachine/countDownStateMachine.ino
+++ countDownStateMachine/countDownStateMachine.ino
@@ -1,9 +1,12 @@
 const int buttonPin = 10;                     // the number of the pushbutton pin
+const int redLED = 2;
+const int yellowLED = 3;
+const int greenLED = 4;
 const unsigned long debounceDuration = 50;    // to avoid button double pressing
 
 int buttonState = 0;                          // variable for reading the pushbutton status
 int buttonHeld = 0;                           // previous pushbutton state
-enum states{waiting, counting, paused};       // possible state the timer can be in
+enum states {waiting, counting, paused};      // possible state the timer can be in
 enum states currentState = waiting;           // the state the timer is in
 unsigned long lastDebounceTime = 0;           // timestamp of last pushbutton press
 unsigned long timerEnd = 0;                   // Time when the timer is up
@@ -13,25 +16,27 @@ bool userInput = false;                       // The user  has put input on the 
 class Pomodoro {
   private:
     int currentTimerStep;
-    const unsigned long times [8] = {5*1000,2*1000,5*1000,2*1000,5*1000,2*1000,5*1000,8*1000};
-    const String TimerStep [8] = {"work 1", "rest 1", "work 2", "rest 2","work 3","rest 3","work 4","long rest"};
-
+    int shortRestTime = 2 * 1000;
+    int longRestTime = 8 * 1000;
+    int workTime = 5 * 1000;    
+    const unsigned long times [8] = {workTime, shortRestTime, workTime, shortRestTime, workTime, shortRestTime, workTime, longRestTime};
+    const String TimerStep [8] = {"work 1", "rest 1", "work 2", "rest 2", "work 3", "rest 3", "work 4", "long rest"};
   public:
     Pomodoro() {
       currentTimerStep = 0;
     }
-    
-    String GetCurrentTimerStep(){
+
+    String GetCurrentTimerStep() {
       return TimerStep[currentTimerStep];
     }
 
-    unsigned long GetCurrentTimerDuration(){
+    unsigned long GetCurrentTimerDuration() {
       return times[currentTimerStep];
     }
 
-    void NextTimerStep(){
+    void NextTimerStep() {
       currentTimerStep++;
-      if (currentTimerStep>7){
+      if (currentTimerStep > 7) {
         currentTimerStep = 0;
       }
     }
@@ -46,77 +51,105 @@ class Timer {
       endTime = 0;
     }
 
-    bool TimerComplete(){
-      if (millis() > endTime){
+    bool TimerComplete() {
+      if (millis() > endTime) {
         return true;
       }
       return false;
     }
-  
-    unsigned long GetRemaining(){
+
+    unsigned long GetRemaining() {
       return (endTime - millis());
     }
-    
-    void SetTimer(unsigned long newDuration){
-        endTime = millis() + newDuration;
+
+    void SetTimer(unsigned long newDuration) {
+      endTime = millis() + newDuration;
     }
 };
+
 Pomodoro pomodoro = Pomodoro();
 Timer timer = Timer();
 
-bool CheckButton(){                           // returns true if valid button press
-  if(lastDebounceTime + debounceDuration < millis()){
+void onLED() {
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  int currentLED = 0;
+  switch(currentState){
+    case waiting:
+      currentLED = yellowLED;
+      break;
+    case counting:
+      currentLED = greenLED;
+      break;
+    case paused:
+      currentLED = redLED;
+      break;
+    }
+  digitalWrite(currentLED, HIGH);
+}
+
+bool CheckButton() {                          // returns true if valid button press
+  if (lastDebounceTime + debounceDuration < millis()) {
     buttonState = digitalRead(buttonPin);
     if (buttonState != buttonHeld) {
       buttonHeld = buttonState;
       lastDebounceTime = millis();
-      if (buttonState == 1){
+      if (buttonState == 1) {
         return true;
       }
     }
-  }  
+  }
   return false;
 }
 
 void setup() {
   pinMode(buttonPin, INPUT);                  // initialize the pushbutton pin as an input:
   Serial.begin(9600);                         // send and receive at 9600 baud
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  onLED();
 }
 
 void loop() {
-   userInput = CheckButton();                 // has button been pressed this cycle
-   switch (currentState){                     // what state is it currently in?
+  userInput = CheckButton();                 // has button been pressed this cycle
+  switch (currentState) {                    // what state is it currently in?
     case waiting:                             // standby state, its just waiting for the user to start the timer
-      if (userInput){                         // if in standby mode and the user presses the button
+      if (userInput) {                        // if in standby mode and the user presses the button
         Serial.print("Starting timer for ");     // inform timer started
         Serial.println(pomodoro.GetCurrentTimerStep());
         currentState = counting;
+        onLED();
         timer.SetTimer (pomodoro.GetCurrentTimerDuration());
       }
       break;
     case counting:                            // the countdown timer is counting down
-      if (timer.TimerComplete()){                 // check if the timer is done
+      if (timer.TimerComplete()) {                // check if the timer is done
         Serial.println("timer done");         // if done, inform user
         currentState = waiting;               // return to standby state
-        pomodoro.NextTimerStep(); 
-      } 
-      else if (userInput){                    // if the button was pressed
+        onLED();
+        pomodoro.NextTimerStep();
+      }
+      else if (userInput) {                   // if the button was pressed
         Serial.print("Pausing timer at ");    // inform that timer is paused
         timeRemaining = timer.GetRemaining();     // record how much time is left on the counter
         Serial.println(timeRemaining);        // inform how much time is left
         currentState = paused;                // go to the paused state
-        }
+        onLED();
+      }
       break;
     case paused:                              // paused state is similar to standby state
-      if (userInput){                         // if button pressed
+      if (userInput) {                        // if button pressed
         Serial.println("Restarting timer");   // inform user that timer restarted
         currentState = counting;              // next state will be counting state
         timer.SetTimer(timeRemaining);
+        onLED();
       }
       break;
     default:                                  // should never get to default state
       Serial.println("default");              // spam the serial
       break;
-    }
-    userInput = false;                        // set button pressed to false as all actions are complete
+  }
+  userInput = false;                        // set button pressed to false as all actions are complete
 }
